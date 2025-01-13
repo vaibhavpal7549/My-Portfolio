c18e3d58508224a9e592549bdbbaadaddc37d406
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2025-01-13 11:09:00 +0530

    Update script.js

--- experience/script.js
+++ experience/script.js
@@ -35,7 +35,8 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+// s1.src='https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+s1.src='https://embed.tawk.to/67847d6049e2fd8dfe067aa1/1iheojlvm';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
